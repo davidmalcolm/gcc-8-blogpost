3a8953a3b42f7c0e3669f381e8a0e598130b7bc1
David Malcolm <dmalcolm@redhat.com>
2018-02-16 08:26:03 -0500

    Introduce a Writer class

--- make-blog.py
+++ make-blog.py
@@ -6,39 +6,44 @@ from pygments import highlight
 from pygments.lexers import guess_lexer_for_filename
 from pygments.formatters import HtmlFormatter
 
-def invoke_gcc(cmd, helper_script):
-    out = subprocess.check_output(['bash', helper_script, cmd],
-                                  stderr=subprocess.PIPE)
-    assert 'class=' not in out
-    print(out)
+class Writer:
+    def write(self, text):
+        sys.stdout.write(text)
 
-def include_source(path):
-    print('<pre>')
-    with open(path) as f:
-        code = f.read()
-    lexer = guess_lexer_for_filename(path, code)
-    sys.stdout.write(highlight(code, lexer, HtmlFormatter(noclasses=True)))
-    print('</pre>')
+    def invoke_gcc(self, cmd, helper_script):
+        out = subprocess.check_output(['bash', helper_script, cmd],
+                                      stderr=subprocess.PIPE)
+        assert 'class=' not in out
+        self.write(out + '\n')
 
-def handle_file(path):
-    with open(path) as f:
-        for line in f:
-            m = re.match('INVOKE_GCC (.*)', line)
-            if m:
-                invoke_gcc(m.group(1), './gcc-to-html.sh')
-                continue
-            m = re.match('INVOKE_OLD_GCC (.*)', line)
-            if m:
-                invoke_gcc(m.group(1), './old-gcc-to-html.sh')
-                continue
-            m = re.match('INCLUDE (.*)', line)
-            if m:
-                handle_file(m.group(1))
-                continue
-            m = re.match('INCLUDE_SOURCE (.*)', line)
-            if m:
-                include_source(m.group(1))
-                continue
-            sys.stdout.write(line)
+    def include_source(self, path):
+        self.write('<pre>\n')
+        with open(path) as f:
+            code = f.read()
+        lexer = guess_lexer_for_filename(path, code)
+        self.write(highlight(code, lexer, HtmlFormatter(noclasses=True)))
+        self.write('</pre>\n')
 
-handle_file(sys.argv[1])
+    def handle_file(self, path):
+        with open(path) as f:
+            for line in f:
+                m = re.match('INVOKE_GCC (.*)', line)
+                if m:
+                    self.invoke_gcc(m.group(1), './gcc-to-html.sh')
+                    continue
+                m = re.match('INVOKE_OLD_GCC (.*)', line)
+                if m:
+                    self.invoke_gcc(m.group(1), './old-gcc-to-html.sh')
+                    continue
+                m = re.match('INCLUDE (.*)', line)
+                if m:
+                    self.handle_file(m.group(1))
+                    continue
+                m = re.match('INCLUDE_SOURCE (.*)', line)
+                if m:
+                    self.include_source(m.group(1))
+                    continue
+                self.write(line)
+
+w = Writer()
+w.handle_file(sys.argv[1])
