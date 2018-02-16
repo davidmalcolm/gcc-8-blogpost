6ddbaf8269c857e0146c4b7382cd948a79660196
David Malcolm <dmalcolm@redhat.com>
2018-02-16 10:07:54 -0500

    Use premailer to properly bake CSS rules into HTML

--- make-blog.py
+++ make-blog.py
@@ -6,13 +6,35 @@ from pygments import highlight
 from pygments.lexers import guess_lexer_for_filename
 from pygments.formatters import HtmlFormatter
 
+import premailer # pip install premailer
+
 class Writer:
+    def __init__(self):
+        cmd = ['/home/david/coding-3/gcc-git-bugfixing/src/ansi2html.sh',
+               '--bg=dark', '--palette=solarized', '--css-only']
+        self.css = subprocess.check_output(cmd,
+                                           stderr=subprocess.PIPE)
+
+    def apply_css(self, text):
+        """Bake CSS rules into text"""
+        p = premailer.Premailer(html=text, css_text=self.css,
+                                remove_classes=True,
+                                remove_unset_properties=True,
+                                disable_leftover_css=True)
+        out = p.transform()
+        for tag in ['html', 'head', 'body']:
+            out = out.replace('<%s>' % tag, '')
+            out = out.replace('</%s>' % tag, '')
+        return out
+
     def write(self, text):
-        sys.stdout.write(text)
+        sys.stdout.write(text.encode('utf-8'))
 
     def invoke_gcc(self, cmd, helper_script):
         out = subprocess.check_output(['bash', helper_script, cmd],
                                       stderr=subprocess.PIPE)
+        out = out.decode('utf-8')
+        out = self.apply_css(out)
         assert 'class=' not in out
         self.write(out + '\n')
 
